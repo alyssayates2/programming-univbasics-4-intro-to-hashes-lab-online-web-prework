0a91977037bbef06f3d5dedac0ac8601819d1ee6
Alyssa Yates <bosslys@icloud.com>
2019-08-22 02:48:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  read_from_hash{name: 'Steve'}# return the correct value using the hash and key parameters
+  key{name: 'Steve'}# return the correct value using the hash and key parameters
 end
 
 def update_counting_hash(hash, key)
